6613ca6856d9fd1e71f814f08f269be5fafd7471
mangG907 <mang.gee@gmail.com>
2024-08-17 16:25:42 +0900

    2021만 다운

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -4,5 +4,5 @@ def test_save_movies():
     #for i in range(2015, 2022):
         #r = save_movies(year=i, sleep_time=0.1)
         #assert r
-    r = save_movies(year=2017, sleep_time=0.1)
+    r = save_movies(year=2021, sleep_time=0.1)
     assert r
